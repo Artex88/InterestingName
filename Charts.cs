dda46489ceed85c8700a089172ec91d8d5647ecd
Artex88 <116633226+Artex88@users.noreply.github.com>
2022-10-25 18:52:30 +0500

    Add files via upload

---
+++ Charts.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Names
+{
+    internal static class Charts
+    {
+        public static void ShowHistogram(HistogramData stats)
+        {
+            // Графики строятся сторонней библиотекой ZedGraph.
+            // Примеры можно найти тут https://jenyay.net/Programming/ZedGraph или тут https://web.archive.org/web/20051127161423/http://zedgraph.sourceforge.net/samples.html
+            // Не бойтесь экспериментировать с кодом самостоятельно!
+
+            var chart = new ZedGraphControl
+            {
+                Dock = DockStyle.Fill
+            };
+            chart.GraphPane.Title.Text = stats.Title;
+            chart.GraphPane.YAxis.Title.Text = "Y";
+            chart.GraphPane.AddBar("", Enumerable.Range(0, stats.YValues.Length).Select(i => (double) i).ToArray(),
+                stats.YValues, Color.Blue);
+            chart.GraphPane.YAxis.Scale.MaxAuto = true;
+            chart.GraphPane.YAxis.Scale.MinAuto = true;
+            chart.GraphPane.XAxis.Type = AxisType.Text;
+            chart.GraphPane.XAxis.Scale.TextLabels = stats.XLabels;
+
+            chart.AxisChange();
+            // Form — это привычное нам окно программы.
+            // Это одна из главных частей подсистемы под названием Windows Forms http://msdn.microsoft.com/ru-ru/library/ms229601.aspx
+            var form = new Form
+            {
+                Text = stats.Title,
+                Size = new Size(800, 600)
+            };
+            form.Controls.Add(chart);
+            form.ShowDialog();
+        }
+    }
+}
---
+++ InterestingTask.cs
@@ -0,0 +1,38 @@
+﻿using Names;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassiveTask
+{
+    internal class HistogramNew
+    {
+        public static HistogramData GetHistogramBirthsByLeapsYears(NameData[] names)
+        {
+            List<int> years = new List<int>();
+            char[] vowels = new char[10] { 'а', 'у', 'о', 'ы', 'э', 'я', 'ю', 'ё', 'и', 'е' };
+            foreach (var name in names)
+            {
+                var year = name.BirthDate.Year;
+                if ((year % 4 == 0 ^ (year % 100 == 0 && year % 400 != 0))  && !years.Contains(name.BirthDate.Year))
+                    years.Add(name.BirthDate.Year);
+            }
+            years.Sort();
+            double[] countBornPeopleNameFirstLetterK = new double[years.Count];
+            foreach (var name in names)
+            {
+                if (!vowels.Contains(name.Name[0]) && years.Contains(name.BirthDate.Year))
+                    countBornPeopleNameFirstLetterK[years.IndexOf(name.BirthDate.Year)]++;
+            }
+            string[] b = new string[years.Count];
+            for (int i = 0; i < years.Count; i++)
+            {
+                b[i] = years[i].ToString();
+            }
+
+            return new HistogramData("Рождаемость людей в высокосные года с именами, которые начинаются с согласной буквы", b, countBornPeopleNameFirstLetterK);
+        }
+    }
+}
